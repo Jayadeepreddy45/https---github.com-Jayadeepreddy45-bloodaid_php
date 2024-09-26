635737189cc7e205b06d70801ec84c867d54ad21
Jayadeepreddy45 <Jayadeepreddy452002@gmail.com>
2024-09-26 18:42:30 +0530

    cleanup

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
+$db_server = "ls-8351950865b689bfff6492a24882a37e8a8e80df.cgd4aj6sswr0.us-east-1.rds.amazonaws.com";
+$db_user = "jayadeep";
+$db_pass = "p@ssw0rd";
 $db_name = "bloodaid";
 $conn = "";
 
